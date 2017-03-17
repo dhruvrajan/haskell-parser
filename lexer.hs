becf5f8f3eed6acbc97e410b803e6267d78c33e2
Dhruv Rajan <dhruv@krishnaprem.com>
2017-03-17 07:51:48 -0700

    added semicolon split

--- lexer.hs
+++ lexer.hs
@@ -5,10 +5,11 @@
 module Lexer where
 import Data.Char
 
-data Statement = Assignment String Integer deriving (Eq, Show)
+data Statement = Assignment String String deriving (Eq, Show)
 
 -- Generic split
 split :: (Char -> Bool) -> String -> (String, String)
+split _ "" = ("", "")
 split key (x:xs) | key x = let (a, b) = split key xs in (x : a, b)
                  | isSpace x = split key xs
                  | otherwise = ("", (x:xs))
@@ -25,6 +26,17 @@ splitEq = split (\x -> x == '=')
 splitNum :: String -> (String, String)
 splitNum = split isDigit
 
--- get next assignment
+-- split on semicolon
+splitSemi :: String -> (String, String)
+splitSemi = split (\x -> x == ';')
 
+-- get next assignment
+assignment :: String -> (Maybe Statement, String)
+assignment prog = if valid then (Just (Assignment var num), remains) else (Nothing, remains)
+  where
+    (var, prog0)   = splitId prog
+    (_, prog1)     = splitEq prog0
+    (num, prog2)   = splitNum prog1
+    (_, remains)   = splitSemi prog2
+    valid = (num /= "") && (var /= "")  
 
