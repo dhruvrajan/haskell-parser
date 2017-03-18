3edbb0f087c6e27127548868ce24fcc1830ca8dc
Dhruv Rajan <dhruv@krishnaprem.com>
2017-03-18 11:36:10 -0700

    removed generic split; using span

--- Lexer.hs
+++ Lexer.hs
@@ -12,30 +12,23 @@ data Token = Variable String | Number Int
 data Statement = Assignment String Int
   | Declaration String deriving (Eq, Show)
 
-type Program = [Maybe Statement]
-
--- Generic split
-split :: (Char -> Bool) -> String -> (String, String)
-split _ "" = ("", "")
-split key (x:xs) | key x = let (a, b) = split key xs in (x : a, b)
-                 | isSpace x = split key xs
-                 | otherwise = ("", (x:xs))
+type Program = [Statement]
 
 -- Return (var, remaining)
 splitId :: String -> (String, String)
-splitId = split isAlpha
+splitId = span isAlpha
 
 -- Split on equals sign
 splitEq :: String -> (String, String)
-splitEq = split (\x -> x == '=')
+splitEq = span (\x -> x == '=')
 
 -- Split on number
 splitNum :: String -> (String, String)
-splitNum = split isDigit
+splitNum = span isDigit
 
 -- split on semicolon
 splitSemi :: String -> (String, String)
-splitSemi = split (\x -> x == ';')
+splitSemi = span (\x -> x == ';')
 
 -- tokenize properly
 tokenize :: String -> (Token, String)
@@ -59,44 +52,9 @@ tokens xs = t : tokens ts
     (t, ts) = tokenize xs
 
 
-
+-- Parse a list of tokens
 parse :: [Token] -> [Statement]
 parse [] = []
 parse (Variable var : Equals : Number val : Semicolon : toks)
   = (Assignment var val) : parse toks
 parse (t:ts) = parse ts -- LLK parsing error recovery
-
-
-
-
-
--- statement :: String -> (Maybe Statement, String)
--- statement tokens = result
---   where
---     -- Perform splits
---     (var, prog0)    = splitId tokens
---     (eq, prog1)     = splitEq prog0
---     (num, prog2)    = splitNum prog1
---     (semi, remains) = splitSemi prog2
-
---     -- Calculate result
---     varExists = var /= ""
---     eqExists  = eq  /= ""
---     numExists = num /= ""
---     semiExists = semi /= ""
---     result = if (varExists && semiExists)
---                 then if (eqExists && numExists)
---                         then (Just (Assignment var num), remains)
---                         else (Just (Declaration var), remains)
---                  else (Nothing, tokens)
-
--- -- parse the whole program
--- program :: String -> Maybe Program
--- program ""   = Just []
--- program prog = if (isNothing rest) then Nothing else Just (statement0 : (fromJust rest))
---   where
---     line = statement prog
---     (statement0, remaining) = line
---     rest0 = program remaining
---     rest = if isNothing statement0  then Nothing else rest0
-
