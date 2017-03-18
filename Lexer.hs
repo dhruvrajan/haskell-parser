e5d3086c173608add66e2ac55497de3ceb5331d1
Dhruv Rajan <dhruv@krishnaprem.com>
2017-03-18 11:10:22 -0700

    added parse method

--- Lexer.hs
+++ Lexer.hs
@@ -6,10 +6,11 @@ module Lexer where
 import Data.Char
 import Data.Maybe
 
-data Statement = Assignment String String
-  | Declaration String deriving (Eq, Show)
+data Token = Variable String | Number Int
+  | Equals | Semicolon | Invalid String deriving (Eq, Show)
 
-data Token = Variable String | Number Int | Equals | Semicolon deriving (Eq, Show)
+data Statement = Assignment String Int
+  | Declaration String deriving (Eq, Show)
 
 type Program = [Maybe Statement]
 
@@ -37,53 +38,65 @@ splitSemi :: String -> (String, String)
 splitSemi = split (\x -> x == ';')
 
 -- tokenize properly
-tokenize :: String -> (Maybe String, String)
-tokenize "" = (Nothing, "")
+tokenize :: String -> (Token, String)
+tokenize "" = (Invalid "", "")
 tokenize (x:xs)
-  | isAlpha x = let (tok, remaining) = splitId (x:xs) in (Just tok, remaining)
-  | isDigit x = let (tok, remaining) = splitNum (x:xs) in (Just tok, remaining)
-  | x == ';'  = let (tok, remaining) = splitSemi (x:xs) in (Just tok, remaining)
-  | x == '='  = let (tok, remaining) = splitEq (x:xs) in (Just tok, remaining)
+  | isAlpha x = let (tok, remaining) = splitId (x:xs) in (Variable tok, remaining)
+  | isDigit x = let (tok, remaining) = splitNum (x:xs)
+    in (Number ((read tok) :: Int), remaining)
+  | x == ';'  = let (tok, remaining) = splitSemi (x:xs) in (Semicolon, remaining)
+  | x == '='  = let (tok, remaining) = splitEq (x:xs) in (Equals, remaining)
   | isSpace x = tokenize xs
-  | otherwise = (Nothing, xs) -- Consume error ?
+  | otherwise = (Invalid [x], xs) -- Consume error?
 
 -- Returns an infinite list of tokens from a program string
 ---- Can exploit Lazy Evaluation
 ---- If at a certain point the
-tokens :: String -> [Maybe String]
+tokens :: String -> [Token]
 tokens "" = []
 tokens xs = t : tokens ts
   where 
     (t, ts) = tokenize xs
 
 
-statement :: String -> (Maybe Statement, String)
-statement tokens = result
-  where
-    -- Perform splits
-    (var, prog0)    = splitId tokens
-    (eq, prog1)     = splitEq prog0
-    (num, prog2)    = splitNum prog1
-    (semi, remains) = splitSemi prog2
 
-    -- Calculate result
-    varExists = var /= ""
-    eqExists  = eq  /= ""
-    numExists = num /= ""
-    semiExists = semi /= ""
-    result = if (varExists && semiExists)
-                then if (eqExists && numExists)
-                        then (Just (Assignment var num), remains)
-                        else (Just (Declaration var), remains)
-                 else (Nothing, tokens)
+parse :: [Token] -> [Statement]
+parse [] = []
+parse (Variable var : Equals : Number val : Semicolon : toks)
+  = (Assignment var val) : parse toks
+parse (t:ts) = parse ts -- LLK parsing error recovery
 
--- parse the whole program
-program :: String -> Maybe Program
-program ""   = Just []
-program prog = if (isNothing rest) then Nothing else Just (statement0 : (fromJust rest))
-  where
-    line = statement prog
-    (statement0, remaining) = line
-    rest0 = program remaining
-    rest = if isNothing statement0  then Nothing else rest0
+
+
+
+
+-- statement :: String -> (Maybe Statement, String)
+-- statement tokens = result
+--   where
+--     -- Perform splits
+--     (var, prog0)    = splitId tokens
+--     (eq, prog1)     = splitEq prog0
+--     (num, prog2)    = splitNum prog1
+--     (semi, remains) = splitSemi prog2
+
+--     -- Calculate result
+--     varExists = var /= ""
+--     eqExists  = eq  /= ""
+--     numExists = num /= ""
+--     semiExists = semi /= ""
+--     result = if (varExists && semiExists)
+--                 then if (eqExists && numExists)
+--                         then (Just (Assignment var num), remains)
+--                         else (Just (Declaration var), remains)
+--                  else (Nothing, tokens)
+
+-- -- parse the whole program
+-- program :: String -> Maybe Program
+-- program ""   = Just []
+-- program prog = if (isNothing rest) then Nothing else Just (statement0 : (fromJust rest))
+--   where
+--     line = statement prog
+--     (statement0, remaining) = line
+--     rest0 = program remaining
+--     rest = if isNothing statement0  then Nothing else rest0
 
